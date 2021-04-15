e5a1c8dffb720f15ccc75b87e4edaddbf7086510
Shuo Han <shan50@jhu.edu>
2021-04-15 18:22:38 -0400

    Test avg grad sample

--- tests/test_avg_sample.py
+++ tests/test_avg_sample.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import os
+import torch
 import numpy as np
 from scipy.ndimage import zoom
 import matplotlib.pyplot as plt
 from pathlib import Path
 
 from sssrlib.patches import Patches
-from sssrlib.sample import AvgGradSample
+from sssrlib.sample import AvgGradSample, GradSample
 
 
 def test_avg_sample():
@@ -15,10 +16,9 @@ def test_avg_sample():
     image = zoom(image, (1, 1, 0.4))
     patches = Patches((32, 1, 10), image, named=False)
 
-    dirname = 'results_avg_sample'
+    dirname = 'results_avg_sample/avg'
     os.system('rm -rf %s' % dirname)
     sample = AvgGradSample(patches, 3, voxel_size=(1, 1, 2.5))
-    print(sample.avg_kernel.shape)
     sample.save_figures(dirname)
     indices = sample.sample()
     for ind in indices:
@@ -27,48 +27,21 @@ def test_avg_sample():
         plt.imshow(patch.squeeze())
         fig.savefig(Path(dirname, '%s.png' % ind))
 
-    # assert len(sample._weights_flat) == len(patches)
-    # grads = [sample._gradients[0], sample._gradients[2]]
+    shape = patches.patch_size
+    avg_kernel = np.zeros(shape)
+    avg_kernel[(shape[0] - 1) // 2, (shape[1] - 1) // 2, (shape[2] - 1) // 2] = 1
+    sample = AvgGradSample(patches, 3, voxel_size=(1, 1, 2.5), avg_kernel=avg_kernel)
 
-    # starts = [(ps - 1) // 2 for ps in patches.patch_size]
-    # sum0 = grads[0][starts[0] : starts[0]+patches.xnum,
-    #                 starts[1] : starts[1]+patches.ynum,
-    #                 starts[2] : starts[2]+patches.znum].sum()
-    # sum1 = grads[1][starts[0] : starts[0]+patches.xnum,
-    #                 starts[1] : starts[1]+patches.ynum,
-    #                 starts[2] : starts[2]+patches.znum].sum()
-    # grads[0] = grads[0] / sum0
-    # grads[1] = grads[1] / sum1
-    # grad = grads[0] * grads[1]
-    # for i in range(100):
-    #     ind = int(np.random.uniform(0, len(sample._weights_flat)))
-    #     x, y, z = np.unravel_index(ind, [patches.xnum, patches.ynum, patches.znum])
-    #     assert sample._weights_flat[ind] == grad[x + (patches.patch_size[0] - 1)//2,
-    #                                              y + (patches.patch_size[1] - 1)//2,
-    #                                              z + (patches.patch_size[2] - 1)//2]
+    dirname = 'results_avg_sample/delta'
+    os.system('rm -rf %s' % dirname)
+    sample.save_figures(dirname)
+    sample.calc_sample_weights()
 
-    # sample = GradSample(patches, 3, voxel_size=(1, 1, 2.5), weights_op='or')
-    # dirname = 'results_sample/or'
-    # os.system('rm -rf %s' % dirname)
-    # sample.save_figures(dirname)
+    sample2 = GradSample(patches, 3, voxel_size=(1, 1, 2.5))
+    sample2.calc_sample_weights()
+    assert torch.allclose(sample._weights_flat, sample2._weights_flat)
 
-    # image = zoom(image, (1, 1, 0.4))
-    # patches = Patches((32, 32, 1), image, named=False)
-
-    # dirname = 'results_sample/x'
-    # os.system('rm -rf %s' % dirname)
-    # sample = GradSample(patches, 3, voxel_size=(1, 1, 2.5),
-    #                     use_grads=[True, False, False])
-    # sample.save_figures(dirname)
-    # indices = sample.sample()
-
-    # for ind in indices:
-    #     patch = patches[ind]
-    #     fig = plt.figure()
-    #     plt.imshow(patch.squeeze())
-    #     fig.savefig(Path(dirname, '%s.png' % ind))
-
-    # print('successful')
+    print('successful')
 
 if __name__ == '__main__':
     test_avg_sample()
